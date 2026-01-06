7afa64f39070f64e1751bf6c56cc5874253ca426
Amritha-07 <p.p.amrithakumar@gmail.com>
2026-01-06 23:00:04 +0530

    update-bfs-dfs

--- Search/BreadthFirstSearch.java
+++ Search/BreadthFirstSearch.java
@@ -6,22 +6,29 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class BreadthFirstSearch {
-    static int bfs(HashMap<Integer, ArrayList<Integer>> graph, int n, int data) {
-        boolean[] visited = new boolean[n];
+    static int bfs(HashMap<Integer, ArrayList<Integer>> graph, int data, boolean isSearch) {
+        HashMap<Integer, Boolean> visited = new HashMap<>();
         Queue<Integer> traverse = new LinkedList<Integer>();
-        for(int start = 0; start < n; start++) {
-            if(visited[start] == true) continue;
+
+        for(int start: graph.keySet()) {
+            if(visited.getOrDefault(start, false) == true) continue;
+
             traverse.add(start);
-            visited[start] = true;
-            while (!traverse.isEmpty()) {
+            visited.put(start, true);
+
+            while(!traverse.isEmpty()) {
                 Integer curr = traverse.poll();
-                if (curr == data) return curr;
+                if (isSearch) {
+                    if (curr == data) return curr;
+                } else {
+                    System.out.print(curr + " ");
+                }
                 ArrayList<Integer> neighbours = graph.get(curr);
                 if (neighbours == null) continue;
-                for(Integer node : neighbours) {
-                    if(visited[node] != true) {
+                for(Integer node: neighbours) {
+                    if(!visited.getOrDefault(node, false)) {
+                        visited.put(node, true);
                         traverse.add(node);
-                        visited[node] = true;
                     }
                 }
             }
@@ -32,11 +39,6 @@ public class BreadthFirstSearch {
     public static void main(String[] args) {
         HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Node Length: ");
-        int n = sc.nextInt();
-        System.out.println("Node: ");
-        for(int i = 0; i < n; i++) System.out.print(i + " ");
-        System.out.println("");
         while (true) {
             System.out.print("0. Exit\n1.Add Edge\nOption: ");
             int option = sc.nextInt();
@@ -47,10 +49,6 @@ public class BreadthFirstSearch {
                     int nodeA = sc.nextInt();
                     System.out.print("Node B: ");
                     int nodeB = sc.nextInt();
-                    if (nodeA < 0 || nodeA >= n || nodeB < 0 || nodeB >= n) {
-                        System.out.println("Node does not exist");
-                        continue;
-                    }
                     graph.putIfAbsent(nodeA, new ArrayList<>());
                     graph.get(nodeA).add(nodeB);
                 }
@@ -60,17 +58,19 @@ public class BreadthFirstSearch {
             }
         }
         System.out.println("Edges: "); 
-        for(int i = 0; i < n; i++) {
-            ArrayList<Integer> neighbours = graph.get(i);
+        for(int start: graph.keySet()) {
+            ArrayList<Integer> neighbours = graph.get(start);
             if(neighbours == null) continue;
             for(Integer node : neighbours) {
-                System.out.println(i + "---" + node);
+                System.out.println(start + "---" + node);
             }
         }
-        System.out.println("Search: ");
+        System.out.println("Traverse: ");
+        bfs(graph, 0, false);
+        System.out.println("\nSearch: ");
         System.out.print("Data: ");
         int data = sc.nextInt();
-        int result = bfs(graph, n, data);
+        int result = bfs(graph, data, true);
         System.out.println(result == -1 ? "Data not Found" : result);
     }
 }
--- Search/DepthFirstSearch.java
+++ Search/DepthFirstSearch.java
@@ -5,22 +5,29 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class DepthFirstSearch {
-    static int dfs(HashMap<Integer, ArrayList<Integer>> graph, int n, int data) {
-        boolean[] visited = new boolean[n];
+    static int dfs(HashMap<Integer, ArrayList<Integer>> graph, int data, boolean isSearch) {
+        HashMap<Integer, Boolean> visited = new HashMap<>();
         Stack<Integer> traverse = new Stack<>();
-        for(int start = 0; start < n; start++) {
-            if(visited[start] == true) continue;
+
+        for(int start: graph.keySet()) {
+            if(visited.getOrDefault(start, false) == true) continue;
+
             traverse.push(start);
-            visited[start] = true;
-            while (!traverse.isEmpty()) {
+            visited.put(start, true);
+
+            while(!traverse.isEmpty()) {
                 Integer curr = traverse.pop();
-                if (curr == data) return curr;
+                if (isSearch) {
+                    if (curr == data) return curr;
+                } else {
+                    System.out.print(curr + " ");
+                }
                 ArrayList<Integer> neighbours = graph.get(curr);
                 if (neighbours == null) continue;
-                for(Integer node : neighbours) {
-                    if(visited[node] != true) {
+                for(Integer node: neighbours) {
+                    if(!visited.getOrDefault(node, false)) {
+                        visited.put(node, true);
                         traverse.push(node);
-                        visited[node] = true;
                     }
                 }
             }
@@ -31,12 +38,6 @@ public class DepthFirstSearch {
     public static void main(String[] args) {
         HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Node Length: ");
-        int n = sc.nextInt();
-        System.out.println("Node: ");
-        for(int i = 0; i < n; i++) System.out.print(i + " ");
-        System.out.println("");
-        OUTER: 
         while (true) {
             System.out.print("0. Exit\n1.Add Edge\nOption: ");
             int option = sc.nextInt();
@@ -47,10 +48,6 @@ public class DepthFirstSearch {
                     int nodeA = sc.nextInt();
                     System.out.print("Node B: ");
                     int nodeB = sc.nextInt();
-                    if (nodeA < 0 || nodeA >= n || nodeB < 0 || nodeB >= n) {
-                        System.out.println("Node does not exist");
-                        continue;
-                    }
                     graph.putIfAbsent(nodeA, new ArrayList<>());
                     graph.get(nodeA).add(nodeB);
                 }
@@ -60,17 +57,19 @@ public class DepthFirstSearch {
             }
         }
         System.out.println("Edges: "); 
-        for(int i = 0; i < n; i++) {
-            ArrayList<Integer> neighbours = graph.get(i);
+        for(int start: graph.keySet()) {
+           ArrayList<Integer> neighbours = graph.get(start);
             if(neighbours == null) continue;
             for(Integer node : neighbours) {
-                System.out.println(i + "---" + node);
-            }
+                System.out.println(start + "---" + node);
+            } 
         }
-        System.out.println("Search: ");
+        System.out.println("Traverse: ");
+        dfs(graph, 0, false);
+        System.out.println("\nSearch: ");
         System.out.print("Data: ");
         int data = sc.nextInt();
-        int result = dfs(graph, n, data);
+        int result = dfs(graph, data, true);
         System.out.println(result == -1 ? "Data not Found" : result);
     }
 }
